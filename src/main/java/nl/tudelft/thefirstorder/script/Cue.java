24c272f7c7248ec86470a08878d1fd313d67319c
Martin Koster <c.m.koster@student.tudelft.nl>
2016-04-22 11:11:56 +0200

    Created Script and Cue class basics

---
+++ src/main/java/nl/tudelft/thefirstorder/script/Cue.java
@@ -0,0 +1,12 @@
+package nl.tudelft.thefirstorder.script;
+
+/**
+ * Created by Martin on 22-4-2016.
+ */
+public class Cue {
+
+    public Cue () {
+
+    }
+
+}
---
+++ src/main/java/nl/tudelft/thefirstorder/script/Script.java
@@ -0,0 +1,75 @@
+package nl.tudelft.thefirstorder.script;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Martin on 22-4-2016.
+ */
+public class Script {
+
+    private ArrayList<Cue> cues;
+
+    /**
+     * Create a Script.
+     * @param cues The cues which are already in the script
+     */
+    public Script(ArrayList<Cue> cues) {
+        this.cues = cues;
+    }
+
+    /**
+     * Get the list of cues.
+     * @return the list of cues
+     */
+    public ArrayList<Cue> getCues() {
+        return cues;
+    }
+
+    /**
+     * Add a cue to the end of the script.
+     * @param cue the cue which has to be added
+     */
+    public void addLast(Cue cue) {
+        cues.add(cue);
+    }
+
+    /**
+     * Add a cue to the begin of the script.
+     * @param cue the cue which has to be added
+     */
+    public void addFirst(Cue cue) {
+        cues.add(0,cue);
+    }
+
+    /**
+     * Remove a cue from the script.
+     * @param cue the cue which has to be removed
+     * @return the list of cues without the removed cue
+     */
+    public void removeCue(Cue cue) {
+        cues.remove(cue);
+    }
+
+    /**
+     * Add a cue in after a particular cue.
+     * @param cueBefore the cue after which one has to be added
+     * @param cueAfter the cue which has to be added
+     */
+    public void addAfter(Cue cueBefore, Cue cueAfter) {
+        if(cues.contains(cueBefore)) {
+            ArrayList<Cue> result = new ArrayList<Cue>();
+            for(int i = 0; i < cues.size(); i++) {
+                if(cues.get(i).equals(cueBefore)) {
+                    result.add(cues.get(i));
+                    result.add(cueAfter);
+                } else {
+                    result.add(cues.get(i));
+                }
+            }
+            this.cues = result;
+        } else {
+            addFirst(cueAfter);
+        }
+    }
+
+}
