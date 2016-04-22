55647ab81f2b1fae48d5a2119267ad85f8bb1e19
Hung Nguyen <H.Nguyen-3@student.tudelft.nl>
2016-04-22 12:19:29 +0200

    Move spring application to backend/

---
+++ backend/src/main/java/nl/tudelft/thefirstorder/Main.java
@@ -0,0 +1,10 @@
+package nl.tudelft.thefirstorder;
+
+/**
+ * Created by rubenwiersma on 18/04/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
