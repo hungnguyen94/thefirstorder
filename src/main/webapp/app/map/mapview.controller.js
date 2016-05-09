1372735174af33d457eef60e2f5192169e078602
Christian aan de Wiel <cadw96@gmail.com>
2016-05-09 22:38:59 +0200

    Frontend bijgewerkt en de mapfunctie herschreven

--- src/main/webapp/app/map/mapview.controller.js
+++ src/main/webapp/app/map/mapview.controller.js
@@ -11,19 +11,35 @@
         var vm = this;
         vm.loadCamera = loadCamera;
         vm.loadCamera();
-        
+
         function loadCamera () {
             Camera.query({
 
             }, onSuccess, onError);
-            
+
             function onSuccess(data, headers) {
                 vm.cameras = data;
                 vm.queryCount = vm.totalItems;
+                drawCameras(data);
             }
             function onError(error) {
                 AlertService.error(error.data.message);
             }
         }
+
+        function drawCameras(cameraData) {
+            // Initialize the canvas
+            var canvas = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
+            for (var i = 0; i < cameraData.length; ++i) {
+                ctx.fillStyle = "#00b0ff";
+                var currentCamera = cameraData[i];
+                ctx.beginPath();
+                ctx.arc(currentCamera.x, currentCamera.y, 5,0,2*Math.PI);
+                ctx.stroke();
+                // ctx.fillRect(cameraData[i].x, cameraData[i].y, 3, 3);
+            }
+        }
+
     }
 })();
