4bc16d40484dbb3f1cd7b407ccd5bb50a0e3cc12
Martin Koster <c.m.koster@student.tudelft.nl>
2016-05-10 14:36:19 +0200

    Removed unused code

--- src/main/java/nl/tudelft/thefirstorder/PDFExport.java
+++ src/main/java/nl/tudelft/thefirstorder/PDFExport.java
@@ -8,7 +8,6 @@ import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.Set;
 import java.util.Iterator;
-import java.util.HashSet;
 
 import com.itextpdf.text.Anchor;
 import com.itextpdf.text.BadElementException;
@@ -131,89 +130,7 @@ public class PDFExport {
             }
 
             document.add(table);
-            //Anchor anchor = new Anchor("First Chapter", catFont);
-            //anchor.setName("First Chapter");
 
-            // Second parameter is the number of the chapter
-            //Chapter catPart = new Chapter(new Paragraph(anchor), 1);
-
-            /**Paragraph subPara = new Paragraph("Subcategory 1", subFont);
-            Section subCatPart = catPart.addSection(subPara);
-            subCatPart.add(new Paragraph("Hello"));
-
-            subPara = new Paragraph("Subcategory 2", subFont);
-            subCatPart = catPart.addSection(subPara);
-            subCatPart.add(new Paragraph("Paragraph 1"));
-            subCatPart.add(new Paragraph("Paragraph 2"));
-            subCatPart.add(new Paragraph("Paragraph 3"));
-
-            // add a list
-            createList(subCatPart);
-            Paragraph paragraph = new Paragraph();
-            addEmptyLine(paragraph, 5);
-            subCatPart.add(paragraph);
-
-            // add a table
-            createTable(subCatPart);
-
-            // now add all this to the document
-            document.add(catPart);
-
-            // Next section
-            anchor = new Anchor("Second Chapter", catFont);
-            anchor.setName("Second Chapter");
-
-            // Second parameter is the number of the chapter
-            catPart = new Chapter(new Paragraph(anchor), 1);
-
-            subPara = new Paragraph("Subcategory", subFont);
-            subCatPart = catPart.addSection(subPara);
-            subCatPart.add(new Paragraph("This is a very important message"));
-
-            // now add all this to the document
-            document.add(catPart);**/
-
-        }
-
-        private static void createTable(Section subCatPart)
-            throws BadElementException {
-            PdfPTable table = new PdfPTable(3);
-
-            // t.setBorderColor(BaseColor.GRAY);
-            // t.setPadding(4);
-            // t.setSpacing(4);
-            // t.setBorderWidth(1);
-
-            PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
-            c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(c1);
-
-            c1 = new PdfPCell(new Phrase("Table Header 2"));
-            c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(c1);
-
-            c1 = new PdfPCell(new Phrase("Table Header 3"));
-            c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(c1);
-            table.setHeaderRows(1);
-
-            table.addCell("1.0");
-            table.addCell("1.1");
-            table.addCell("1.2");
-            table.addCell("2.1");
-            table.addCell("2.2");
-            table.addCell("2.3");
-
-            subCatPart.add(table);
-
-        }
-
-        private static void createList(Section subCatPart) {
-            List list = new List(true, false, 10);
-            list.add(new ListItem("First point"));
-            list.add(new ListItem("Second point"));
-            list.add(new ListItem("Third point"));
-            subCatPart.add(list);
         }
 
         private static void addEmptyLine(Paragraph paragraph, int number) {
