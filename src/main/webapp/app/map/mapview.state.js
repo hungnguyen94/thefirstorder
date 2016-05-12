ae7bb1ab3f59bebed6501bfd481f4cac38bd34df
Christian aan de Wiel <cadw96@gmail.com>
2016-05-12 11:12:53 +0200

    Navbar does not show map anymore when you are not authenticated and you get an error page when you put /map directly in the url

--- src/main/webapp/app/map/mapview.state.js
+++ src/main/webapp/app/map/mapview.state.js
@@ -12,7 +12,7 @@
             parent: 'app',
             url: '/maps',
             data: {
-                authorities: []
+                authorities: ['ROLE_USER']
             },
             views: {
                 'content@': {
