4ee28a228e4e07903973ba6284dc082bf7f832c4
Christian aan de Wiel <cadw96@gmail.com>
2016-05-13 00:46:56 +0200

    Added page and canvas for the timeline. Also added authorization to the pages.

--- src/main/webapp/app/timeline/timeline.controller.js
+++ src/main/webapp/app/timeline/timeline.controller.js
@@ -11,8 +11,8 @@
         var vm = this;
         var grid = 15;
 
-        vm.loadCamera = loadCamera;
-        vm.loadCamera();
+        vm.loadCues = loadCues;
+        vm.loadCues();
 
         function loadCues () {
             Cue.query({
@@ -22,7 +22,7 @@
             function onSuccess(data, headers) {
                 vm.cues = data;
                 vm.queryCount = vm.totalItems;
-                drawTimelin(data);
+                drawTimeline(data);
             }
             function onError(error) {
                 AlertService.error(error.data.message);
@@ -30,19 +30,51 @@
         }
 
         function drawTimeline(cues) {
-            var canvas = new fabric.canvas('concertTimeline');
+            var htmlCanvas = document.getElementById('concertTimeline');
+
+            // Resize the timeline to the size of the parent
+            resizeToParent(htmlCanvas);
+
+            var canvas = new fabric.Canvas('concertTimeline');
             var interval = 25;
 
-
+            // Draw the grid of the timeline
+            drawInterval(canvas, interval);
         }
 
-        function drawGrid(canvas, interval) {
+        function drawInterval(canvas, interval) {
             var x = 0;
             var line = null;
+            var rect = [];
 
-            while (x < canvas.width) {
+            for (var i = 0; i < Math.ceil(canvas.width / interval); ++i) {
+                // Instantiate an array representing the line (drawn as rectangle)
+                rect[0] = i * Math.ceil(canvas.width / interval);
+                rect[1] = 0;
+                rect[2] = i * Math.ceil(canvas.width / interval);
+                rect[3] = canvas.height;
+
+                // Overwrite line from the previous loop
                 line = null;
+                line = new fabric.Line(rect, {
+                    stroke: 'black'
+                });
+
+                line.selectable = false;
+                canvas.add(line);
+                line.sendToBack();
             }
+            canvas.renderAll;
+        }
+
+        function resizeToParent(canvas) {
+            // Resize it to the parents' size
+            canvas.style.width = '100%';
+            canvas.style.height = '100%';
+
+            // Now set the internal size, so it will not appear blurry
+            canvas.width = canvas.offsetWidth;
+            canvas.height = canvas.offsetHeight;
         }
     }
 })();
