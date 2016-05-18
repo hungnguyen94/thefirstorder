568e9d346cbf0e76eddda2a012a4981b365a6ce3
Christian aan de Wiel <cadw96@gmail.com>
2016-05-18 20:15:26 +0200

    Changed to visjs to draw the timeline, since the performance of fabricjs is very low

--- src/main/webapp/app/timeline/timeline.controller.js
+++ src/main/webapp/app/timeline/timeline.controller.js
@@ -7,17 +7,11 @@
 
     TimelineController.$inject = ['$scope', '$state', 'Cue', 'AlertService'];
 
-    /**
-     * Controller for the timeline.
-     * @param $scope scope of the timeline
-     * @param $state state of the timeline
-     * @param Cue the cue entity
-     * @param AlertService the alert service.
-     * @constructor
-     */
     function TimelineController ($scope, $state, Cue, AlertService) {
         var vm = this;
-        var grid = 15;
+
+        var width = 120;
+        var height = 60;
 
         vm.loadCues = loadCues;
         vm.loadCues();
@@ -30,72 +24,53 @@
             function onSuccess(data, headers) {
                 vm.cues = data;
                 vm.queryCount = vm.totalItems;
-                drawTimeline(data);
+                // DOM element where the Timeline will be attached
+                var container = document.getElementById('visualization');
+
+                // Create a DataSet using the cues from the database
+                var dataSet = [];
+                for (var i = 0; i < vm.cues.length; ++i) {
+                    var startTime = vm.cues[i].timePoint.startTime;
+                    var endTime = vm.cues[i].timePoint.startTime + vm.cues[i].timePoint.duration;
+
+                    var startYear = '';
+                    var endYear = '';
+                    for (var j = 0; j < 4 - startTime.toString().length; ++j)
+                        startYear += '0';
+
+                    startYear += startTime.toString();
+
+                    for (var j = 0; j < 4 - endTime.toString().length; ++j)
+                        endYear += '0';
+
+                    endYear += endTime.toString();
+
+                    console.log(startYear);
+
+                    dataSet.push({
+                        id: i,
+                        content: "Cue " + i,
+                        start: startYear + "-01-01",
+                        end: endYear + '-01-01'
+                    })
+                }
+
+                var items = new vis.DataSet(dataSet);
+
+                // Configuration for the Timeline
+                var options = {
+                    'timeAxis' : {scale: 'year', step: 1},
+                    'min': '0000-01-01',
+                    'zoomMin': 63072000000,
+                    'zoomMax': 700000000000
+                };
+
+                // Create a Timeline
+                var timeline = new vis.Timeline(container, items, options)
             }
             function onError(error) {
                 AlertService.error(error.data.message);
             }
-        }
-
-        /**
-         * Draws the timeline with given cues.
-         * @param cues the cues to draw
-         */
-        function drawTimeline(cues) {
-            var htmlCanvas = document.getElementById('concertTimeline');
-
-            // Resize the timeline to the size of the parent
-            resizeToParent(htmlCanvas);
-
-            var canvas = new fabric.Canvas('concertTimeline');
-            var interval = 25;
-
-            // Draw the grid of the timeline
-            drawInterval(canvas, interval);
-        }
-
-        /**
-         * Draws interval lines on the timeline.
-         * @param canvas the canvas to draw to
-         * @param interval the distance between lines
-         */
-        function drawInterval(canvas, interval) {
-            var x = 0;
-            var line = null;
-            var rect = [];
-
-            for (var i = 0; i < Math.ceil(canvas.width / interval); ++i) {
-                // Instantiate an array representing the line (drawn as rectangle)
-                rect[0] = i * Math.ceil(canvas.width / interval);
-                rect[1] = 0;
-                rect[2] = i * Math.ceil(canvas.width / interval);
-                rect[3] = canvas.height;
-
-                // Overwrite line from the previous loop
-                line = null;
-                line = new fabric.Line(rect, {
-                    stroke: 'black'
-                });
-
-                line.selectable = false;
-                canvas.add(line);
-                line.sendToBack();
-            }
-            canvas.renderAll;
-        }
-
-        /**
-         * Resizes the canvas to parent size.
-         * @param canvas the canvas to resize
-         */
-        function resizeToParent(canvas) {
-            // Resize it to the parents' size
-            canvas.style.width = '100%';
-            canvas.style.height = '100%';
-
-            // Now set the internal size, so it will not appear blurry
-            canvas.width = canvas.offsetWidth;
-            canvas.height = canvas.offsetHeight;
         }
     }
 })();
