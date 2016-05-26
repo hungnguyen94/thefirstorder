c990d39209ab0244f17d4e8c85855459199610b1
Chris Berg <c.c.berg@student.tudelft.nl>
2016-05-26 16:27:55 +0200

    Added create new project popup

---
+++ src/main/webapp/app/home/create-dialog.controller.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('thefirstorderApp')
+        .controller('CreateDialogController', CreateDialogController);
+
+    CreateDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', '$q', 'entity', 'Project', 'Script', 'Map', 'Player', 'Camera'];
+
+    function CreateDialogController($timeout, $scope, $stateParams, $uibModalInstance, $q, entity, Project, Script, Map, Player, Camera) {
+        var vm = this;
+        vm.project = entity;
+        vm.scripts = Script.query({filter: 'project-is-null'});
+        $q.all([vm.project.$promise, vm.scripts.$promise]).then(function () {
+            if (!vm.project.script || !vm.project.script.id) {
+                return $q.reject();
+            }
+            return Script.get({id: vm.project.script.id}).$promise;
+        }).then(function (script) {
+            vm.scripts.push(script);
+        });
+        vm.maps = Map.query({filter: 'project-is-null'});
+        $q.all([vm.project.$promise, vm.maps.$promise]).then(function () {
+            if (!vm.project.map || !vm.project.map.id) {
+                return $q.reject();
+            }
+            return Map.get({id: vm.project.map.id}).$promise;
+        }).then(function (map) {
+            vm.maps.push(map);
+        });
+        vm.players = Player.query();
+        vm.cameras = Camera.query();
+
+        $timeout(function () {
+            angular.element('.form-group:eq(1)>input').focus();
+        });
+
+        var onSaveSuccess = function (result) {
+            $scope.$emit('thefirstorderApp:projectUpdate', result);
+            $uibModalInstance.close(result);
+            vm.isSaving = false;
+        };
+
+        var onSaveError = function () {
+            vm.isSaving = false;
+        };
+
+        vm.save = function () {
+            vm.isSaving = true;
+            if (vm.project.id !== null) {
+                Project.update(vm.project, onSaveSuccess, onSaveError);
+            } else {
+                Project.save(vm.project, onSaveSuccess, onSaveError);
+            }
+        };
+
+        vm.clear = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }
+})();
--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -21,6 +21,34 @@
                     controllerAs: 'vm'
                 }
             }
-        });
+        })
+            .state('home.create', {
+                parent: 'home',
+                url: '/new',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/home/create-dialog.html',
+                        controller: 'CreateDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {
+                                    name: null,
+                                    id: null
+                                };
+                            }
+                        }
+                    }).result.then(function () {
+                        $state.go('mapview', null, {reload: true});
+                    }, function () {
+                        $state.go('home');
+                    });
+                }]
+            });
     }
 })();
