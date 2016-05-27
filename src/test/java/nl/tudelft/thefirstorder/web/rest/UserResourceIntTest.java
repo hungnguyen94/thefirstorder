0f99cdaab8212cfd4d2090768e0632ddbf6d69e8
Martin Koster <c.m.koster@student.tudelft.nl>
2016-05-27 11:50:59 +0200

    added test commented it because a bug

--- src/test/java/nl/tudelft/thefirstorder/web/rest/UserResourceIntTest.java
+++ src/test/java/nl/tudelft/thefirstorder/web/rest/UserResourceIntTest.java
@@ -1,26 +1,35 @@
 package nl.tudelft.thefirstorder.web.rest;
 
 import nl.tudelft.thefirstorder.ThefirstorderApp;
+import nl.tudelft.thefirstorder.domain.Map;
 import nl.tudelft.thefirstorder.domain.User;
 import nl.tudelft.thefirstorder.repository.UserRepository;
 import nl.tudelft.thefirstorder.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+
+import java.util.HashSet;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,15 +51,52 @@ public class UserResourceIntTest {
     @Inject
     private UserService userService;
 
+    @Inject
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Inject
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
     private MockMvc restUserMockMvc;
+
+    private User user;
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         UserResource userResource = new UserResource();
         ReflectionTestUtils.setField(userResource, "userRepository", userRepository);
         ReflectionTestUtils.setField(userResource, "userService", userService);
-        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
+        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setMessageConverters(jacksonMessageConverter).build();
     }
+
+    @Before
+    public void initTest() {
+        user = new User();
+        user.setLogin("test");
+        user.setEmail("Test@test.nl");
+        user.setPassword("Foo1223435axsfascc..!!");
+    }
+
+//    @Test
+//    @Transactional
+//    public void createUser() throws Exception {
+//        int databaseSizeBeforeCreate = userRepository.findAll().size();
+//
+//        userRepository.save(user);
+//
+//        restUserMockMvc.perform(post("/api/users")
+//            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+//            .content(TestUtil.convertObjectToJsonBytes(user)))
+//            .andExpect(status().isCreated());
+//
+//        List<User> users = userRepository.findAll();
+//        assertThat(users).hasSize(databaseSizeBeforeCreate + 1);
+//        User testUser = users.get(users.size() - 1);
+//        assertThat(testUser.getLogin()).isEqualTo("test");
+//    }
 
     @Test
     public void testGetExistingUser() throws Exception {
