5a1d671a852a27fadf66dc3d3da4c834732e00be
rubenwiersma <r.t.wiersma@student.tudelft.nl>
2016-06-03 11:22:19 +0200

    Improved timeline controller code

--- src/main/webapp/app/scripting/scripting.controller.js
+++ src/main/webapp/app/scripting/scripting.controller.js
@@ -5,7 +5,7 @@
         .module('thefirstorderApp')
         .controller('ScriptingController', ScriptingController);
 
-    ScriptingController.$inject = ['$rootScope', '$scope', '$state', 'Camera', 'Script', 'Cue', 'AlertService'];
+    ScriptingController.$inject = ['$rootScope', '$scope', '$state', 'Map', 'Cue', 'AlertService'];
 
     /**
      * The controller for the script view.
@@ -15,7 +15,7 @@
      * @param AlertService the alertservice
      * @constructor
      */
-    function ScriptingController ($rootScope, $scope, $state, Camera, Script, Cue, AlertService) {
+    function ScriptingController ($rootScope, $scope, $state, Map, Cue, AlertService) {
         var vm = this;
 
         vm.saved = true;
@@ -23,129 +23,137 @@
             vm.saved = false;
         }
 
-        vm.loadCameras = loadCameras;
-        vm.loadCues = loadCues;
-        vm.loadCameras();
-        vm.loadScripts = Script.query();
+        vm.cues = Cue.query();
+        $scope.$watch('vm.map', function (newMap) {
+            console.log('Changed map:', newMap);
+            drawTimeline($scope.vm.map);
+        });
+        
+        /**
+         * Draws the timeline with all the cues.
+         */
+        function drawTimeline(map) {
+            var container = document.getElementById('visualization');
 
-        function loadCameras () {
-            Camera.query({
+            var groups = createGroups(map);
+            var items = createItems();
 
-            }, onSuccess, onError);
+            var options = {
+                groupOrder: function (a, b) {
+                    return a.value - b.value;
+                },
+                timeAxis : {scale: 'year', step: 1},
+                min: '0000-01-01',
+                start: '0000-01-01',
+                end: '0010-01-01',
+                zoomMin: 63072000000,
+                zoomMax: 700000000000,
+                editable: true,
+                stack: false,
+                itemsAlwaysDraggable: true,
+                onAdd: onAdd,
+                onUpdate: onUpdate,
+                onMove: unSaved,
+                onRemove: unSaved
+            };
 
-            function onSuccess(data, headers) {
-                vm.cameras = data;
-                vm.queryCount = vm.totalItems;
-                vm.loadCues(data);
-            }
-            function onError(error) {
-                AlertService.error(error.data.message);
-            }
+            // Create a Timeline
+            var timeline = new vis.Timeline(container);
+            timeline.setOptions(options);
+            timeline.setGroups(groups);
+            timeline.setItems(items);
+            timeline.addCustomTime('0000-01-01', 'scroller');
         }
 
-        function loadCues (cameraData) {
-            Cue.query({
-
-            }, onSuccess, onError);
-
-            function onSuccess(data, headers) {
-                vm.cues = data;
-                vm.queryCount = vm.totalItems;
-                // DOM element where the Timeline will be attached
-                var container = document.getElementById('visualization');
-
-                // Create groups
-                var groupsArray = [];
-                for (var i = 0; i < cameraData.length; ++i) {
-                    var classnr = cameraData[i].id % 3 + 1;
-                    groupsArray.push({
-                        content: cameraData[i].name,
-                        id: cameraData[i].name,
-                        value: cameraData[i].id,
-                        className: "camera" + classnr
-                    })
-                }
-                var groups = new vis.DataSet(groupsArray);
-
-
-                // Create a DataSet using the cues from the database
-                var dataSet = [];
-                for (var i = 0; i < vm.cues.length; ++i) {
-                    var startTime = vm.cues[i].bar;
-                    var endTime = vm.cues[i].bar + vm.cues[i].duration;
-
-                    var startYear = parseIntAsYear(startTime);
-                    var endYear = parseIntAsYear(endTime);
-
-                    dataSet.push({
-                        id: vm.cues[i].id,
-                        content: vm.cues[i].action,
-                        start: startYear + "-01-01",
-                        end: endYear + '-01-01',
-                        group: vm.cues[i].camera.name
-                    })
-                }
-
-                var items = new vis.DataSet(dataSet);
-
-                // Configuration for the Timeline
-                var options = {
-                    groupOrder: function (a, b) {
-                        return a.value - b.value;
-                    },
-                    timeAxis : {scale: 'year', step: 1},
-                    min: '0000-01-01',
-                    start: '0000-01-01',
-                    end: '0010-01-01',
-                    zoomMin: 63072000000,
-                    zoomMax: 700000000000,
-                    editable: true,
-                    stack: false,
-                    itemsAlwaysDraggable: true,
-                    onAdd: onAdd,
-                    onUpdate: onUpdate,
-                    onMove: unSaved,
-                    onRemove: unSaved
-                };
-
-                // Create a Timeline
-                var timeline = new vis.Timeline(container);
-                timeline.setOptions(options);
-                timeline.setGroups(groups);
-                timeline.setItems(items);
-                timeline.addCustomTime('0000-01-01', 'scroller');
-            }
-            function onError(error) {
-                AlertService.error(error.data.message);
-            }
-
-            function onAdd(item, callback) {
-                vm.saved = false;
-                $state.go('scripting.new');
-                var endyear = item.start.getFullYear() + 5;
-                item.end = endyear + '-01-01';
-                $rootScope.$on('cueadded', function (event, args) {
-                    item.content = args.cuename;
-                    callback(item);
+        /**
+         * Creates a vis.DataSet with all the groups for the timeline.
+         * @returns vis.DataSet with timeline groups
+         */
+        function createGroups(map) {
+            var groupsArray = [];
+            map.cameras.forEach(function (camera) {
+                var classNumber = camera.id % 3 + 1;
+                groupsArray.push({
+                    content: camera.name,
+                    id: camera.name,
+                    value: camera.id,
+                    className: "camera" + classNumber
                 });
-            }
+            });
+            return new vis.DataSet(groupsArray);
+        }
 
-            function onUpdate(item, callback) {
-                vm.saved = false;
-                $state.go('scripting.update', {name: item.content});
-                $rootScope.$on('cueupdated', function (event, args) {
-                    item.content = args.cuename;
-                    callback(item);
-                    item = null;
-                });
-            }
+        /**
+         * Creates a vis.DataSet with all the cues to draw to the timeline.
+         * @returns vis.DataSet with cues
+         */
+        function createItems() {
+            var dataSet = [];
+            vm.cues.forEach(function (cue) {
+                var startTime = cue.bar;
+                var endTime = cue.bar + cue.duration;
 
-            function unSaved(item, callback) {
-                vm.saved = false;
+                var startYear = parseIntAsYear(startTime);
+                var endYear = parseIntAsYear(endTime);
+
+                dataSet.push({
+                    id: cue.id,
+                    content: cue.action,
+                    start: startYear + "-01-01",
+                    end: endYear + '-01-01',
+                    group: cue.camera.name
+                })
+            });
+
+            return new vis.DataSet(dataSet);
+        }
+
+        /**
+         * Moves to the scripting.new state and updates item on input from that state.
+         * @param item the item that is updated
+         * @param callback the callback to the vis.js draw function
+         */
+        function onAdd(item, callback) {
+            vm.saved = false;
+            $state.go('scripting.new');
+            var endyear = item.start.getFullYear() + 5;
+            item.end = endyear + '-01-01';
+            $rootScope.$on('cueadded', function (event, args) {
+                item.content = args.cuename;
                 callback(item);
-            }
+            });
         }
 
+        /**
+         * Moves to the scripting.update state and updates item on input from that state.
+         * @param item the item that is updated
+         * @param callback the callback to the vis.js draw function
+         */
+        function onUpdate(item, callback) {
+            vm.saved = false;
+            $state.go('scripting.update', {name: item.content});
+            $rootScope.$on('cueupdated', function (event, args) {
+                item.content = args.cuename;
+                callback(item);
+                item = null;
+            });
+        }
+
+        /**
+         * Sets the save state to unsaved.
+         * @param item the item that was updated
+         * @param callback the callback to the vis.js draw function
+         */
+        function unSaved(item, callback) {
+            vm.saved = false;
+            callback(item);
+        }
+
+        /**
+         * Parses an int and returns the correct string for date parsing.
+         * @param year the year to parse
+         * @returns a string with a year
+         */
         function parseIntAsYear(year) {
             var current = "";
 
