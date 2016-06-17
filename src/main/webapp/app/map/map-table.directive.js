7cbe17f64e67bc142d3e64a04fca177e9744ac89
Chris Berg <c.c.berg@student.tudelft.nl>
2016-06-17 15:37:18 +0200

    Added double click messages to map page

--- src/main/webapp/app/map/map-table.directive.js
+++ src/main/webapp/app/map/map-table.directive.js
@@ -5,26 +5,53 @@
         .module('thefirstorderApp')
         .directive('mapTableView', mapTableView);
 
-    mapTableView.$inject = ['FabricMapDialog'];
+    mapTableView.$inject = ['FabricMapDialog', '$timeout'];
 
     /**
      * The controller for the map table view. Shows all cameras and players in a table.
      * @param $uibModal
      * @returns {{restrict: string, scope: {map: string, selected: string}, templateUrl: string, link: link, controller: string, controllerAs: string, bindToController: boolean}}
      */
-    function mapTableView(FabricMapDialog) {
+    function mapTableView(FabricMapDialog, $timeout) {
         var directive = {
             restrict: 'EA',
             scope: {
                 'map': '=',
                 'selected': '='
             },
+            link: link,
             templateUrl: 'app/map/map-table-view-template.html',
             controller: 'MapEditorController',
             controllerAs: 'vm',
             bindToController: true
         };
         return directive;
+
+        /**
+         * Links the controller with the directive.
+         * @param scope the scope that the directive is called in
+         * @param element the element that the directive is called in
+         * @param attrs the attributes of the element that the directive is called in
+         */
+        function link(scope, element, attrs) {
+            scope.vm.hasCameras = true;
+            scope.vm.hasPlayers = true;
+
+            scope.$watch('vm.map', function (map) {
+                $timeout(function () {
+                    if(map.cameras.length > 0){
+                        scope.vm.hasCameras = true;
+                    } else {
+                        scope.vm.hasCameras = false;
+                    }
+
+                    if(map.players.length > 0){
+                        scope.vm.hasPlayers = true;
+                    } else {
+                        scope.vm.hasPlayers = false;
+                    }
+                });
+            });
+        }
     }
 })();
-
