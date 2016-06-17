d3b50d5ddb40fc36ae3df1bfe4cc5003f693fcb2
Hung Nguyen <H.Nguyen-3@student.tudelft.nl>
2016-06-17 11:08:25 +0200

    Removed add player and camera button from tableview

--- src/main/webapp/app/map/map-table.directive.js
+++ src/main/webapp/app/map/map-table.directive.js
@@ -20,33 +20,11 @@
                 'selected': '='
             },
             templateUrl: 'app/map/map-table-view-template.html',
-            link: link,
             controller: 'MapEditorController',
             controllerAs: 'vm',
             bindToController: true
         };
         return directive;
-
-        /**
-         * Links the controller to this directive.
-         * @param scope the scope that the directive is called in
-         * @param element the element that the directive is called in
-         * @param attrs the attributes of the element that the directive is called in
-         */
-        function link(scope, element, attrs) {
-            scope.vm.addCamera = function(map) { dialogCallback(FabricMapDialog.addCamera(map)) };
-            scope.vm.addPlayer = function(map) { dialogCallback(FabricMapDialog.addPlayer(map)) };
-            scope.vm.deleteCamera = function(cameraId) { dialogCallback(FabricMapDialog.deleteCamera(cameraId)) };
-            scope.vm.deletePlayer = function(playerId) { dialogCallback(abricMapDialog.deletePlayer(playerId)) };
-
-            function dialogCallback(func) {
-                console.log('dialog func', func);
-                func.result.then(function () {
-                    scope.vm.update();
-                })
-            }
-
-        }
     }
 })();
 
